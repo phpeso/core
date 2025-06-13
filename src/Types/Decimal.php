a68434ea07eab8536923baddee98545b5322ef6a
Anton Smirnov <sandfox+git@sandfox.me>
2025-06-13 17:05:07 +0300

    Test Decimal

--- src/Types/Decimal.php
+++ src/Types/Decimal.php
@@ -10,14 +10,24 @@ use ValueError;
 
 final readonly class Decimal
 {
-    public function __construct(
-        public string $value,
-    ) {
-        if (!preg_match('/^-?\d*\.\d*$/', $value)) {
+    public string $value;
+
+    /**
+     * @param numeric-string $value
+     */
+    public function __construct(string $value) {
+        if (!preg_match('/^-?\d*\.?\d*$/', $value)) {
             throw new ValueError('$value must be a string representing a decimal number');
         }
+        if ($value === '.') {
+            $value = '0';
+        }
+        $this->value = $value;
     }
 
+    /**
+     * @param numeric-string|Decimal|Number|BigDecimal $value
+     */
     public static function init(string|Decimal|Number|BigDecimal $value): self
     {
         if ($value instanceof Decimal) {
---
+++ tests/DecimalTypeTest.php
@@ -0,0 +1,55 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Peso\Core\Tests;
+
+use BcMath\Number;
+use Brick\Math\BigDecimal;
+use Peso\Core\Types\Decimal;
+use PHPUnit\Framework\TestCase;
+use ValueError;
+
+class DecimalTypeTest extends TestCase
+{
+    public function testAcceptsString(): void
+    {
+        self::assertEquals('12.34', (new Decimal('12.34'))->value);
+        self::assertEquals('1234', (new Decimal('1234'))->value);
+        self::assertEquals('.34', (new Decimal('.34'))->value);
+        self::assertEquals('12.', (new Decimal('12.'))->value);
+        self::assertEquals('0', (new Decimal('.'))->value);
+        self::assertEquals('-12.34', (new Decimal('-12.34'))->value);
+    }
+
+    public function testNonNumeric(): void
+    {
+        self::expectException(ValueError::class);
+        self::expectExceptionMessage('$value must be a string representing a decimal number');
+
+        new Decimal('abc.de');
+    }
+
+    public function testAcceptsAnotherDecimal(): void
+    {
+        $decimal = new Decimal('12.34');
+
+        self::assertEquals($decimal, Decimal::init($decimal));
+    }
+
+    public function testAcceptsBcMath(): void
+    {
+        if (PHP_VERSION_ID < 80400) {
+            $this->markTestSkipped('PHP 8.4+ only');
+        }
+
+        $number = new Number('12.34');
+        self::assertEquals('12.34', Decimal::init($number)->value);
+    }
+
+    public function testAcceptsBrick(): void
+    {
+        $number = BigDecimal::of('12.34');
+        self::assertEquals('12.34', Decimal::init($number)->value);
+    }
+}
