a9154bc0477fe8ef1c02a61acc15ce46fb34206c
Anton Smirnov <sandfox+git@sandfox.me>
2025-06-30 21:03:02 +0300

    ConversionNotPerformedException

---
+++ src/Exceptions/ConversionNotPerformedException.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Peso\Core\Exceptions;
+
+use Peso\Core\Requests\CurrentConversionRequest;
+use Peso\Core\Requests\HistoricalConversionRequest;
+use Throwable;
+
+final class ConversionNotPerformedException extends PesoResponseException
+{
+    public static function fromRequest(
+        CurrentConversionRequest|HistoricalConversionRequest $request,
+        Throwable|null $previous = null
+    ): self {
+        return new self(match (true) {
+            $request instanceof CurrentConversionRequest => \sprintf(
+                'Unable to convert %s %s to %s',
+                $request->baseAmount->value,
+                $request->baseCurrency,
+                $request->quoteCurrency,
+            ),
+            $request instanceof HistoricalConversionRequest => \sprintf(
+                'Unable to convert %s %s to %s on %s',
+                $request->baseAmount->value,
+                $request->baseCurrency,
+                $request->quoteCurrency,
+                $request->date->toString(),
+            ),
+        }, previous: $previous);
+    }
+}
